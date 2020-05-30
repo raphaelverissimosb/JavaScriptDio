2f8a8728a13089207b6f43ae627fb84d87bd072d
Raphael Veríssimo <62971242+raphaelverissimosb@users.noreply.github.com>
2020-05-30 09:03:47 -0300

    my first commands in JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+
+/*
+var nome = "Raphael Verissimo";
+var idade = 29;
+var frase =  "Alemanha e o melhor time do mundo";
+console.log(frase.replace("Alemanha","Japao"))
+alert("my JS" + nome + " tem " + idade);
+alert(frase.replace("Japao","Brazil"));
+*/
+
