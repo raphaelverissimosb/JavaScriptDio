2c148498999bca28b14d570873ddf65eddb6d000
Raphael Veríssimo <62971242+raphaelverissimosb@users.noreply.github.com>
2020-05-30 09:42:13 -0300

    do, for and while on JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 
+var nome = prompt("Qual e o seu nome ?");
 
+if(nome == "Alan"){
+    alert("Seu nome realmente e :" + nome);
+}else{
+    alert("Desculpe , nome nao cadastrado em noso banco de dados!");
+}
+
+
+
+/*
+var lista = ["pera","uva","maca"];
+console.log(lista)
+lista.push("salada-mista");
+console.log(lista)
+console.log(typeof(lista));
+console.log(lista.length);
+
+
+var lista2 = {nome1:'Raphael',nome2:"Thiago"};
+console.log(lista2);
+console.log(lista2[1]);
+
+
+var lista3 = [{nome:"Marlon",nome:"Larissa"},{nome:"Pedro",nome:"Karina"},{nome:"Guliver",nome:"Shelsea"}];
+*/
 
 /*
 var nome = "Raphael Verissimo";
